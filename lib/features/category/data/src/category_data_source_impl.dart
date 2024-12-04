20bf15314904914d622d08d4c4e7e4e12142fd26
Ceren Canbaz <ceren_canbaz@hotmail.com>
2024-12-04 14:04:26 +0300

    Injectable package added and build.yaml created'

--- lib/features/category/data/src/category_data_source_impl.dart
+++ lib/features/category/data/src/category_data_source_impl.dart
@@ -1,10 +1,1 @@
-class FirebaseCategoryRepository implements CategoryRepository {
-  final CategoryDataSource dataSource;
 
-  FirebaseCategoryRepository({required this.dataSource});
-
-  @override
-  Future<List<Category>> getCategories() async {
-    return await dataSource.getCategories();
-  }
-}
